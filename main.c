0b31b0ebb4720f4eeb7549f476141e3c219bbda5
ohwk123 <seojeong@sookmyung.ac.kr>
2022-11-15 16:30:03 +0900

    02

--- main.c
+++ main.c
@@ -4,14 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void) {
-	int i = 4;
+	
+	int i=300;
 	
 	int *pi = &i;
 	char *pc = &i;
 	
 	printf("%i, %i, %i\n", i, *pi, *pc);
-	
-	
-	
 
 }
