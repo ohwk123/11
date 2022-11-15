8e33d3d57c52ee80dc33e773ad768a60bea3f473
ohwk123 <seojeong@sookmyung.ac.kr>
2022-11-15 17:00:53 +0900

    05

--- main.c
+++ main.c
@@ -5,22 +5,19 @@
 
 
 int main(int argc, char *argv[]) {
-	int i;
-	int grade[5];
-	int sum=0;
-
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for(i=0;i<5;i++)
-	{
-		printf("%i : %i\n", i,*(grade+i));
-		sum += *(grade+i);
-	}
+	pc = (char*)10000;
+	pi = (int*)10000;
+	pd = (double*)10000;
 	
-	printf("average: %i", sum/5);
+	printf("증가 전:pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후:pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
 }
